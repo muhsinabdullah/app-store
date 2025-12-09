cae9abc63afa10b97744b369c26ce8a774526d5b
muhsinabdullah <muhsinabdullah1250@gmail.com>
2025-12-10 01:30:10 +0600

    Banner Added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaAppStoreIos, FaGooglePlay } from 'react-icons/fa';
+import bannerImg from '../assets/hero.png'
+const Banner = () => {
+    return (
+        <div>
+            <div>
+                <h1 className=' text-6xl font-black text-center mt-8'>We Build <br />
+                    <span className='bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-purple-500 font-bold'>  Productive </span> Apps</h1>
+                <p className=' text-[#627382] text-center mt-5'>At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+            </div>
+            <div className="flex items-center justify-center mt-5 gap-3">
+                <button className="btn flex items-center gap-2">
+                    <FaGooglePlay />
+                    Google Play
+                </button>
+
+                <button className="btn flex items-center gap-2">
+                    <FaAppStoreIos />
+                    App Store
+                </button>
+            </div>
+
+            <div className="mt-5 flex justify-center">
+                <img
+                    src={bannerImg}
+                    alt=""
+                    className="w-full max-w-md object-cover"
+                />
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
